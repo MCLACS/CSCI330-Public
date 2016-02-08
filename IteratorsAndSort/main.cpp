7381faf0381151792cd3c5ed7c515a794d46dc61
mclacsdev <mclacsdev@mclacsdev-VirtualBox.(none)>
2016-02-08 17:28:09 -0500

    added iterators and sort example

--- IteratorsAndSort/main.cpp
+++ IteratorsAndSort/main.cpp
@@ -1,4 +1,4 @@
-#include <iterator>
+#include <algorithm>
 #include <iostream>
 #include <vector>
 #include <cstring>
@@ -8,17 +8,42 @@ using namespace std;
 
 int main()
 {
-  const char * ary = "MCLA ALL THE WAY!";
-  vector<char> vChars;
-  string sChars;
+  char ary[] = "MCLA ALL THE WAY!";
+	vector<char> vChars(ary, ary+strlen(ary));
+  string sChars(ary);
 
-  const char * start = ary;
-  const char * end = (ary+strlen(ary));
-  for (const char * p = start; p != end; p++)
-  {
-    sChars += *p;
-    vChars.push_back(*p);
-  }
+  char * abegin = ary;
+  char * aend = (ary+strlen(ary));
+  for (char * p = abegin; p != aend; p++)
+		cout << *p;
+	cout << endl;
+
+	vector<char>::iterator vbegin = vChars.begin();
+	vector<char>::iterator vend = vChars.end();
+	for (vector<char>::iterator p = vbegin; p != vend; p++)
+		cout << *p;
+	cout << endl;
+
+	string::iterator sbegin = sChars.begin();
+	string::iterator send = sChars.end();
+	for (string::iterator p = sbegin; p != send; p++)
+		cout << *p;
+	cout << endl;  
+
+	sort(ary, ary+strlen(ary));
+  for (char * p = abegin; p != aend; p++)
+		cout << *p;
+	cout << endl;
+
+	sort(vbegin, vend);
+	for (vector<char>::iterator p = vbegin; p != vend; p++)
+		cout << *p;
+	cout << endl;
+
+	sort(sbegin, send);
+	for (string::iterator p = sbegin; p != send; p++)
+		cout << *p;
+	cout << endl;  
 
   return 0;
 }

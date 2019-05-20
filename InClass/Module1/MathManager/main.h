edc48bdb88a9f807db83d126a16ac39269721b1b
MarK <mark.cohen@mcla.edu>
2019-05-20 12:56:55 -0400

    cleanup module 1

--- InClass/Module1/MathManager/main.h
+++
@@ -1,9 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-const int SIZE(5);
-
-void readNumbers(double ary[], int size);
-void printNumbers(double ary[], int size);
-
-#endif
--- InClass/Module1/PinGuess/main.h
+++
@@ -1,7 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-bool checkPin(int pin[], int guess[], int length);
-void guessPin(int guess[], int length);
-
-#endif
